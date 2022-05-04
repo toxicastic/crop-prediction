3fe53f58325fe51349cccf9329b19986b8e012ce
toxicastic <krpunjabi3210@gmail.com>
2022-05-04 14:57:35 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Crop-Advisory-for-Farmers
+
+<b>Problem Statement:</b> Agriculture/Farming is the backbone of our country, sufficing the needs of over 1.3 billion people. Today one of the most tragic challenge which farmers of our country face is to determine which crop is most suitable and profitable for his land, for his soil.
+
+My own personal experience: I come from a farmer family. My uncle owns a 25 acre land. Last year he invested in rice and today he has a debt of around 10 lakhs. Why this is happening? This is happening because Weather and soil attributes change over time and farmer today relies either on past experience or advises of other farmers. This is leading them to great loses.
+
+<p align="center">
+<img src="https://github.com/pdesai878/Crop-Advisory-for-Farmers/blob/main/images/Farmer.png">
+</p>
+<br><br><hr>
+  
+So we intend to make a system which will solve the above issue.
+<br><br><hr>
+
+<b> SYSTEM ARCHITECTURE </b>
+<img src="https://github.com/pdesai878/Crop-Advisory-for-Farmers/blob/main/images/System%20Architecture.png">
+<br><br> <hr> 
+
+<table>
+<h3>
+
+<th> <b> Key Technologies: </b></th>
+
+
+<tr> <td><b> Programming Language: </b></td>
+<td> Python</td></tr>
+
+<tr> <td><b> Framework: </b></td>
+<td> Streamlit </td></tr>
+
+<tr> <td><b> Web Technologies: </b></td>
+<td> HTML5 </td>
+<td> CSS3 </td>
+</tr>
+<tr> <td><b> Modelling: </b></td>
+<td> Scikit-learn </td></tr>
+tr> <td><b> Data Analysis: </b></td>
+<td> Pandas </td>
+<td> Numpy </td></tr>
+</table>
+</h3>
+<br><br> <hr>
+  
+
+<b> SCREENSHOT </b>
+<img src="https://github.com/pdesai878/Crop-Advisory-for-Farmers/blob/main/images/Screenshot.png">
+<br><br><hr>
+  
+  
+
+# streamlit-to-heroku-tutorial
+# streamlit-to-heroku-tutorial
---
+++ app.py
@@ -0,0 +1,110 @@
+import streamlit as st
+import numpy as np 
+import pandas as pd 
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingRegressor, AdaBoostClassifier
+import streamlit.components.v1 as components
+
+
+html = """
+  <style>
+  u{
+  text-decoration-line: underline;
+  text-decoration-style: double;
+  }
+
+  mark{
+  
+  font-size:110%;
+  color: black;
+
+  }
+
+
+
+  font{
+   color: grey;
+  }
+
+  embed:focus {
+  outline: none;
+}
+  border-radius:50%;
+    
+  </style>
+"""
+
+
+
+
+# crop Advisory for Farmers
+
+st.markdown('<style>' + open('css/title.css').read() + '</style>', unsafe_allow_html=True)
+
+
+st.markdown(html, unsafe_allow_html=True)
+r1=pd.read_csv(r"D:\Crop-Advisory-for-Farmers-main\Dataset\r1.csv")
+crop_data=pd.read_csv(r"D:\Crop-Advisory-for-Farmers-main\Dataset\crop_data.csv")
+st.markdown('<div class="title">कृषि उपज मार्गदर्शक 🌾 </div>', unsafe_allow_html=True)
+
+st.sidebar.write('<div class="subheader-sidebar">राज्य की पसंदगी :</div>',unsafe_allow_html=True)	
+State_list=list(r1['state'].unique())
+State=st.sidebar.selectbox("राज्य : ",r1['state'].unique())
+attributes=[]
+
+
+#rainfall prediction
+st.write('<div class="subheader">Predicted rainfall in your state in 2020 :<br>[अनुमानित बारिश  वर्ष 2020 में] </div>',unsafe_allow_html=True)
+x=pd.DataFrame(r1,columns=['le_state','le_year'])
+y=pd.DataFrame(r1,columns=['rainfall_avg'])
+lr=GradientBoostingRegressor(learning_rate=0.1,n_estimators=246,min_samples_leaf=80,random_state=40).fit(x,y)
+
+if State:
+	pred_data=[[State_list.index(State),120]]
+	#rainfall prediction
+	pred=lr.predict(pred_data)
+	strr_=str(pred)
+	strr_=strr_.replace('[[','')
+	strr_=strr_.replace(']]','')
+
+	st.write("<mark> %s mm</mark>" %strr_,unsafe_allow_html=True)
+	st.sidebar.write('<div class="subheader-sidebar">पाक की विगत : </div>',unsafe_allow_html=True)	
+	Season_list=list(crop_data['Season'].unique())
+	Season=st.sidebar.selectbox("ऋतु : ",crop_data['Season'].unique())
+	ph=st.sidebar.number_input("PH का मूल्य : ",0.5,8.5,0.5)
+	n=st.sidebar.number_input("नाइट्रोजन का प्रमाण : ",2.0,90.0,2.0)
+	p=st.sidebar.number_input("फोस्फरस का प्रमाण : ",1.0,50.0,1.0)
+	k=st.sidebar.number_input("पोटेसियम का प्रमाण: ",0.5,20.0,0.5)
+	# ph=st.sidebar.number_input("Enter PH content: ",0.5,8.5,0.5)
+	# n=st.sidebar.number_input("Enter Nitrogen content: ",2.0,90.0,2.0)
+	# p=st.sidebar.number_input("Enter Phosphorous content: ",1.0,50.0,1.0)
+	# k=st.sidebar.number_input("Enter Potassium content: ",0.5,20.0,0.5)
+	# temp=st.sidebar.number_input("Enter temperature: ",2.0,50.0,2.0)
+	temp=st.sidebar.number_input("तापमान : ",2.0,50.0,2.0)
+	st.write('<div class="subheader">Most profitable crop for you :<br> [सबसे फ़ायदेकिय फसल] </div>',unsafe_allow_html=True )
+
+	x_=crop_data[['Season_le','ph_avg','n','p','k','avg_temp','rainfall_avg']]
+	y_=crop_data.Crop
+	clf=AdaBoostClassifier(learning_rate=0.146731,n_estimators=18,random_state=40).fit(x_,y_)
+	i=Season_list.index(Season)
+	predictors=[i,ph,n,p,k,temp,pred]
+	str_=str(clf.predict([predictors]))
+	#st.write(str(clf.predict([predictors])))
+	str_=str_.replace('[\'','')
+	str_=str_.replace('\']','')
+
+	
+	# st.subheader(str_)
+	st.write("<mark>%s</mark>" % str_,unsafe_allow_html=True)
+
+	
+
+	if st.button("फसल की जानकारी"):
+		if str_=="Arhar/Tur":
+			st.image(r"D:\Crop-Advisory-for-Farmers-main\data_insights/Arhar_Tur.png", width=800)
+		st.image(r"D:\Crop-Advisory-for-Farmers-main\data_insights/%s.png" % str_, width=800)
+
+		
+
+
